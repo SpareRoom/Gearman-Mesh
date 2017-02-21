33a2236f04f5fcbcd2bc65cfd9306516ef2bed97
Mike Raynham <mike.raynham@spareroom.co.uk>
2017-02-21 07:31:52 +0000

    Version 0.4

--- lib/Gearman/Mesh/Worker.pm
+++ lib/Gearman/Mesh/Worker.pm
@@ -41,11 +41,11 @@ Gearman::Mesh::Worker - A wrapper around Gearman::XS::Worker
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
